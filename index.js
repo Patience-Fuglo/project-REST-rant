0deed3b1ac573f8626e7b51022da08b7ce716420
Patience-Fuglo <pfuglo@fordham.edu>
2023-02-11 19:16:13 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('*', (req, res) => {
+  res.status(404).send('<h1>404 Page</h1>')
+})
 
 const PORT = process.env.PORT || 7000
 
