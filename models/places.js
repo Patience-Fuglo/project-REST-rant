0a307e368a9c9de2ae8bb18ae62dd9120fc1f53b
Patience-Fuglo <pfuglo@fordham.edu>
2023-02-26 00:54:28 -0500

    Added mongoose.Schema

--- models/places.js
+++ models/places.js
@@ -1,29 +1,12 @@
-// require mongoose
 const mongoose = require("mongoose");
 
-// creating Schema constructor
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
   city: { type: String, default: "Anytown" },
   state: { type: String, default: "USA" },
-  cuisines: { type: String, required: true },
-  pic: {
-    type: String,
-    default: "/images/thai-food.jpg",
-  },
-  founded: {
-    type: Number,
-    min: [1673, "Surely not that old?!"],
-    max: [new Date().getFullYear(), "Hey, this year is in the future!"],
-  },
-  rating: { type: String, default: "Not Rated" },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  founded: Number,
 });
 
-// Helper methods
-placeSchema.methods.showEstablished = function () {
-  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`;
-};
-
-// model and export
 module.exports = mongoose.model("Place", placeSchema);
